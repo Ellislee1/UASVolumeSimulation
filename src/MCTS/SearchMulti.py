d7a9b2891b4de350c7cc1a49831284476513133e
Ellis Thompson <EllisThompson.1999@outlook.com>
2022-04-24 15:32:36 -0400

    Added Tree Search

---
+++ src/MCTS/SearchMulti.py
@@ -0,0 +1,25 @@
+"""
+MCTS approach addapted from the work by Xuxi Yang (https://github.com/xuxiyang1993/Multi_MCTS_Guidance_Separation_Assurance/blob/master/MCTS/common.py)
+"""
+
+class MCTS:
+    def __init__(self, node):
+        self.root = node
+
+    def best_action(self, simulations, search_depth):
+        for _ in range(simulations):
+            v = self.tree_policy(search_depth)
+            reward = v.rollout(search_depth)
+            v.backpropagate(reward)
+
+        return self.root.best_child(c_param = 0.)
+
+    def tree_policy(self, search_depth):
+        current_node = self.root
+
+        while not current_node.is_terminal_node(search_depth):
+            if not current_node.is_fully_expanded():
+                return current_node.expand()
+            else:
+                current_node = current_node.best_child()
+        return current_node
